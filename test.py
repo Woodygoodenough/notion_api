42cdd09ee3ba740e697d8119bae2d8b816a5fd5d
woody_win_laptop <woodygoodenough@outlook.com>
2023-08-16 16:54:59 +0800

    tempsave

--- test.py
+++ test.py
@@ -1,47 +1,59 @@
 import os
 import requests
-import re
 import json
 
 NOTION_TOKEN = os.environ.get("NOTION_KEY")
-NOTION_PAGE_ID = "aaa18f4dfc56495e835e0289cbe25f3b"
+NOTION_PAGE_ID = "79abdc9bdbc14a1488ae0297bc756145"
 NOTION_VERSION = "2022-06-28"
-HEADERS = {"Authorization": f"Bearer {NOTION_TOKEN}", "Notion-Version": NOTION_VERSION}
+HEADERS = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Notion-Version": NOTION_VERSION,
+    "Content-Type": "application/json",
+}
 
 
-# Fetch all children of a Notion block or page
-def fetch_all_notion_block_children(block_id):
-    url = f"https://api.notion.com/v1/blocks/{block_id}/children"
+def fetch_data_from_notion(page_id, debug_mode=False):
+    """
+    Fetches data from a Notion page and returns a list of an exhaustive list of all blocks (including nested ones)
+    on the page.
+    """
     all_results = []
 
-    i = 0
-    while True:
-        response = requests.get(url, headers=HEADERS)
+    def fetch_notion_block_children_recursive(block_id):
+        url = f"https://api.notion.com/v1/blocks/{block_id}/children"
 
-        if response.status_code != 200:
-            print(f"Failed to fetch Notion block children. Status code: {response.status_code}")
-            break
+        while True:
+            response = requests.get(url, headers=HEADERS)
 
-        data = response.json()
-        i += 1
-        with open(f".response_page_{i}.json", "w") as f:
-            json.dump(data, f, indent=4)
+            if response.status_code != 200:
+                print(f"Failed to fetch Notion block children. Status code: {response.status_code}")
+                break
 
-        # Add the results from this chunk to the all_results list
-        all_results.extend(data["results"])
+            data = response.json()
 
-        # If there's more data to fetch
-        if data["has_more"]:
-            # Set the URL for the next request
-            url = f"https://api.notion.com/v1/blocks/{block_id}/children?start_cursor={data['next_cursor']}"
-        else:
-            # If there's no more data, break the loop
-            break
+            # Add the results from this chunk to the all_results list
+            all_results.extend(data["results"])
+
+            # If there's more data to fetch
+            if data["has_more"]:
+                url = f"https://api.notion.com/v1/blocks/{block_id}/children?start_cursor={data['next_cursor']}"
+            else:
+                # If no more data to fetch from the current block, check its children for nested blocks.
+                # Recursively fetch data from these nested blocks and add to the all_results list.
+                for result in data["results"]:
+                    if result["has_children"]:
+                        fetch_notion_block_children_recursive(result["id"])
+                break
+
+    fetch_notion_block_children_recursive(page_id)
+
+    if debug_mode:
+        with open(".results.json", "w") as f:
+            json.dump(all_results, f, indent=4)
 
     return all_results
 
 
-# Extract units from content
 def extract_units_from_content(results):
     units = []
 
@@ -55,13 +67,44 @@ def extract_units_from_content(results):
     return units
 
 
+def get_the_data_base_info(database_id):
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+    # Filter by the 'contexts' property having a value (is not empty)
+    data = {
+        "filter": {
+            "or": [
+                {"property": "Tags", "contains": "contexts"},
+            ]
+        }
+    }
+
+    data_1 = {
+        "filter": {
+            "or": [
+                {"property": "In stock", "checkbox": {"equals": True}},
+                {"property": "Cost of next trip", "number": {"greater_than_or_equal_to": 2}},
+            ]
+        },
+        "sorts": [{"property": "Last ordered", "direction": "ascending"}],
+    }
+
+    response = requests.post(url, headers=HEADERS, json=data)
+
+    print(json.dumps(response.json(), indent=4))
+
+    if response.status_code != 200:
+        print(f"Failed to query database. Status code: {response.status_code}")
+        return None
+
+
 if __name__ == "__main__":
-    results = fetch_all_notion_block_children(NOTION_PAGE_ID)
+    debug_mode = False  # Set this to False if you don't want to save to a JSON file
+    """
+    all_results = fetch_data_from_notion(NOTION_PAGE_ID, debug_mode)
 
-    # load content into a json file for test purpose
-    with open(".content.json", "w") as f:
-        json.dump(results, f, indent=4)
-
-    if results:
-        units = extract_units_from_content(results)
+    if all_results:
+        units = extract_units_from_content(all_results)
         print(units)
+    """
+    get_the_data_base_info(NOTION_PAGE_ID)
