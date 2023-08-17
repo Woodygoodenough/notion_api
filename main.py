c50b14fc4c9bf7d87966b1c7a4c93a9c8f1b0e1b
woody <woodygoodenough@outlook.com>
2023-08-17 22:32:11 +0800

    tempsave

---
+++ main.py
@@ -0,0 +1,30 @@
+from notion_api_utils import *
+from wm_api_utils import *
+
+
+def from_contexts_to_units():
+    debug_mode = True
+    unfold_block("7650f74465864a788ed4dc7ba92131c6", debug_mode=debug_mode)
+    with open(".results.json", "r") as f:
+        blocks = json.load(f)
+    units = extract_units_from_blocks(blocks)
+    print(json.dumps(units, indent=4))
+    target_block_id = "0ec528b8540a44e9b01d4e596000fc84"
+    for unit, source_id in units.items():
+        source_page_id, source_block_id = [clean_id(id) for id in source_id[0]]
+        append_linked_block_to_target_block(target_block_id, unit, source_block_id, source_page_id)
+        print(f"https://www.notion.so/{source_page_id}#{source_block_id}")
+
+
+def create_pages_for_units(word):
+    words_database_id = "a9d64a44ea8844088612055786f85954"
+    word_response = get_word_mw_response(word)
+    if word_response["error"]:
+        print(word_response["message"])
+        return None
+    word_data = word_response["data"]
+    create_notion_page_for_a_word(word_data, words_database_id)
+
+
+if __name__ == "__main__":
+    create_pages_for_units("idiosyncratic")
---
+++ wm_api_utils.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import json
+from settings import DEBUG
+
+# Your Merriam-Webster API Key
+MW_API_KEY = os.environ.get("MERRIAM_WEBSTER_KEY")
+
+# Endpoint for the Merriam-Webster Collegiate Dictionary
+MW_BASE_URL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
+
+
+def get_word_mw_response(word):
+    # Construct the URL
+    url = f"{MW_BASE_URL}{word}?key={MW_API_KEY}"
+
+    # Make the request
+    response = requests.get(url)
+
+    # Extract data from the response
+    data = response.json() if response.status_code == 200 else None
+    # DEBUG mode
+    if DEBUG:
+        with open(".word_mw_response.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+    # Check if the request was successful and the data is a list
+    if not data or not isinstance(data, list):
+        return {
+            "error": True,
+            "message": f"Error fetching data or unexpected data format: status_code = {response.status_code}; data = {data}",
+        }
+    if "shortdef" not in data[0]:
+        return {
+            "error": True,
+            "message": f"Error, most likely word not found and list of suggestions returned: data = {data}",
+        }
+
+    return {"error": False, "data": data[0]}
+
+
+if __name__ == "__main__":
+    # Test the function
+    word_to_lookup = "good"  # Replace with any word you want to look up
+    print(f"Definition of {word_to_lookup}: {get_word_mw_response(word_to_lookup)}")
